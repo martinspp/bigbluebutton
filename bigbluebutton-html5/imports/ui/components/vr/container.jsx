12e5dd02867f0961b1d6310661eb68d50637f035
s9_prelga_m <19413900+martinspp@users.noreply.github.com>
2022-03-28 22:10:52 +0300

    asdfasdf

--- bigbluebutton-html5/imports/ui/components/vr/container.jsx
+++ bigbluebutton-html5/imports/ui/components/vr/container.jsx
@@ -4,32 +4,26 @@ import VRService from './service'
 import Unity, { UnityContext } from "react-unity-webgl";
 import { withTracker } from 'meteor/react-meteor-data';
 
-const unityContext = new UnityContext({
-  loaderUrl: "vr/build.loader.js",
-  dataUrl: "vr/build.data",
-  frameworkUrl: "vr/build.framework.js",
-  codeUrl: "vr/build.wasm",
-});
+
 
 const VRContainer = (props) =>{
 
   if(VRService.isVRAvailable()){
     return (
-      <VRComponent
-        {
-          ...{
-            ...props
-          }
-        }
-      />
-            
-    )
+      <VRComponent {...props } />
+    );
   }
   return null
 }
 
 export default withTracker(() =>{
+  const unityContext = new UnityContext({
+    loaderUrl: "vr/build.loader.js",
+    dataUrl: "vr/build.data",
+    frameworkUrl: "vr/build.framework.js",
+    codeUrl: "vr/build.wasm",
+  });
   return{
-    unityContext: this.unityContext,
+    unityContext,
   }
 })(VRContainer);
