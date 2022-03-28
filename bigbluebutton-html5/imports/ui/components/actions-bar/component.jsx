78f0f8bdec7455d093f53cc40c1cc212aaba2b68
s9_prelga_m <19413900+martinspp@users.noreply.github.com>
2022-03-29 00:10:19 +0300

    asdfasdf

--- bigbluebutton-html5/imports/ui/components/actions-bar/component.jsx
+++ bigbluebutton-html5/imports/ui/components/actions-bar/component.jsx
@@ -9,7 +9,7 @@ import ScreenshareButtonContainer from '/imports/ui/components/actions-bar/scree
 import AudioControlsContainer from '../audio/audio-controls/container';
 import JoinVideoOptionsContainer from '../video-provider/video-button/container';
 import PresentationOptionsContainer from './presentation-options/component';
-import VRContainer from '/imports/ui/components/vr/container'
+import {unityContext} from '/imports/ui/components/vr/container'
 
 class ActionsBar extends PureComponent {
   render() {
@@ -134,7 +134,7 @@ class ActionsBar extends PureComponent {
                   color="primary"
                   circle
                   onClick={() => {
-                    console.log("help")
+                    console.log(unityContext)
                   }
                     
                   } 
