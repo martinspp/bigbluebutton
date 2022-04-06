f32eb6953cd4322a85c406ea2c50edd1fe6e8f7f
s9_prelga_m <19413900+martinspp@users.noreply.github.com>
2022-04-07 01:10:06 +0300

    asdfasdf

--- bigbluebutton-html5/imports/ui/components/vr/component.jsx
+++ bigbluebutton-html5/imports/ui/components/vr/component.jsx
@@ -1,14 +1,30 @@
 import React, { PureComponent } from "react";
 import { injectIntl } from "react-intl";
 import Unity, { UnityContext } from "react-unity-webgl";
+import {
+  isStreamStateUnhealthy,
+  subscribeToStreamStateChange,
+  unsubscribeFromStreamStateChange,
+} from '/imports/ui/services/bbb-webrtc-sfu/stream-state-service';
 
 class VRComponent extends PureComponent{
 
+  constructor(){
+    super();
+    this.onStreamStateChange = this.onStreamStateChange.bind(this);
+  }
+  
   startVR(){
     const {unityContext} = this.props
     unityContext.startVR()
   }
-
+  
+  componentDidMount(){
+    subscribeToStreamStateChange('screenshare', this.onStreamStateChange);
+  }
+  onStreamStateChange(){
+    console.log("stream started, change in vrcomponent")
+  }
   render(){
     const{
       unityContext
