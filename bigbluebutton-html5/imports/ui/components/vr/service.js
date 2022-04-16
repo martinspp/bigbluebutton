656c7817aa78c00a70e8b4bdd025590f60d8098f
s9_prelga_m <19413900+martinspp@users.noreply.github.com>
2022-04-17 02:04:20 +0300

    asdfasdf

--- bigbluebutton-html5/imports/ui/components/vr/service.js
+++ bigbluebutton-html5/imports/ui/components/vr/service.js
@@ -1,12 +1,12 @@
 const isVRAvailable = async () => {
-    const xrSessionSupported = polyfill.xr.isSessionSupported()
-    if(xrSessionSupported === undefined){
-        return false;
-    }
-    console.log("here")
-    const ret = await xrSessionSupported.then((val)=>{return val})
-    console.log(ret)
-    return ret
+    //const xrSessionSupported = polyfill.xr.isSessionSupported()
+    //if(xrSessionSupported === undefined){
+    //    return false;
+    //}
+    //console.log("here")
+    //const ret = await xrSessionSupported.then((val)=>{return val})
+    //console.log(ret)
+    return true;
 }
 
 export default{
