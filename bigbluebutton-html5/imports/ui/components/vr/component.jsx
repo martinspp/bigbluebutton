0564c0820587699a7934216a8e746b96a8e14da0
s9_prelga_m <19413900+martinspp@users.noreply.github.com>
2022-04-16 17:36:10 +0300

    asdfasdf

--- bigbluebutton-html5/imports/ui/components/vr/component.jsx
+++ bigbluebutton-html5/imports/ui/components/vr/component.jsx
@@ -1,12 +1,15 @@
 import React, { PureComponent } from "react";
 import { injectIntl } from "react-intl";
 import Unity, { UnityContext } from "react-unity-webgl";
+import { unityContext } from "./container";
 import {
   isStreamStateUnhealthy,
   subscribeToStreamStateChange,
   unsubscribeFromStreamStateChange,
 } from '/imports/ui/services/bbb-webrtc-sfu/stream-state-service';
-
+import Users from '/imports/api/users';
+import Auth from '/imports/ui/services/auth';
+import { data } from "autoprefixer";
 class VRComponent extends PureComponent{
 
   constructor(){
@@ -24,6 +27,14 @@ class VRComponent extends PureComponent{
   }
   onStreamStateChange(){
     console.log("stream started, change in vrcomponent")
+    data = [Auth.authenticateURL(SFU_URL),
+      "none",
+      Auth.userID,
+      Auth.meetingID,
+      Auth.fullname,
+      BridgeService.getConferenceBridge()]
+    console.log("Sending to unity: %O ", data)
+    unityContext.send("GameObject","ScreenshareInit",data)
   }
   render(){
     const{
