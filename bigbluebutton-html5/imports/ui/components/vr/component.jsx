40a916d2345e5a5e44fb0627e164f392a46c0afe
s9_prelga_m <19413900+martinspp@users.noreply.github.com>
2022-04-16 21:14:53 +0300

    asdfasdf

--- bigbluebutton-html5/imports/ui/components/vr/component.jsx
+++ bigbluebutton-html5/imports/ui/components/vr/component.jsx
@@ -50,7 +50,7 @@ class VRComponent extends PureComponent{
     } = this.props;
 
     return (
-      <Unity unityContext={unityContext} />
+      //<Unity unityContext={unityContext} />
     )
   }
 }
