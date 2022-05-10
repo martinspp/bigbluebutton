1a10e64ca86c5f71de79fa0aa4e0079c680b42b6
s9_prelga_m <19413900+martinspp@users.noreply.github.com>
2022-05-11 01:43:18 +0300

    asdfasdf

--- vr-ws/run.sh
+++ vr-ws/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-docker build . --tag vr-ws-server
+if [[ "$(docker images -q vr-ws-server:latest 2> /dev/null)" == "" ]]; then
+  docker build . --tag vr-ws-server
+fi
 docker rm -f vr-ws-server
 docker run -d -p 8765:8765 --name vr-ws-server vr-ws-server
