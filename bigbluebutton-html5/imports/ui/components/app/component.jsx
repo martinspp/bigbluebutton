8f988e3fc3c63e431d99174165e962e4430693a5
s9_prelga_m <19413900+martinspp@users.noreply.github.com>
2022-05-15 04:51:23 +0300

    asdfasdf

--- bigbluebutton-html5/imports/ui/components/app/component.jsx
+++ bigbluebutton-html5/imports/ui/components/app/component.jsx
@@ -514,7 +514,6 @@ class App extends Component {
           <ManyWebcamsNotifier />
           <PollingContainer />
           <ModalContainer />
-          <VRContainer />
           {this.renderActionsBar()}
           {customStyleUrl ? <link rel="stylesheet" type="text/css" href={customStyleUrl} /> : null}
           {customStyle ? <link rel="stylesheet" type="text/css" href={`data:text/css;charset=UTF-8,${encodeURIComponent(customStyle)}`} /> : null}
@@ -524,6 +523,7 @@ class App extends Component {
   }
 }
 //
+//<VRContainer />
 App.propTypes = propTypes;
 App.defaultProps = defaultProps;
 
